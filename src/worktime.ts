5bb0b92c96515688eda9182c37ce17d9f401b06c
otofu-square <yukita@feedforce.jp>
2017-06-19 03:00:29 +0900

    :recycle: Refactor by jquery

--- src/worktime.ts
+++ src/worktime.ts
@@ -1,116 +1,82 @@
+import * as $ from "jquery";
+
 /*
  * Scraping Jobcan employee/attendance page.
  * ft. https://github.com/hanocha/how-many-work-time/blob/master/app/models/work_time_info.rb
  */
-
-var stdWorkDays,
-  stdWorkHours,
-  workedDays,
-  workedHours,
-  salariedDays,
-  remainWorkDays,
-  excessWorkTimes,
-  workTimeMargin,
-  requiredWorkTimes,
-  lastUpdatedAt;
-
-var infotplRow = document
-  .getElementsByClassName("infotpl")[0]
-  .getElementsByTagName("tr"),
-  rowLength = infotplRow.length;
+const infotplRow = $(".infotpl");
+const rowLength = $(".infotpl").length;
 
 // 所定労働日数
-stdWorkDays = ~~infotplRow[4]
-  .getElementsByTagName("td")[0]
-  .innerText.split(" ")[0];
+const stdWorkDays = ~~$(".infotpl td")[3].innerText.split(" ")[0];
+
 // 所定労働時間
-stdWorkHours = stdWorkDays * 8;
+const stdWorkHours = stdWorkDays * 8;
 
 // 実働日数
-workedDays = ~~infotplRow[6].getElementsByTagName("td")[0].innerText;
+const workedDays = ~~$(".infotpl td")[4].innerText;
 
 // 実労働時間
-var tmpWorked = infotplRow[13].getElementsByTagName("td")[0].innerText,
-  tmpWorkedHours = ~~tmpWorked.split(":")[0],
-  tmpWorkedMinutes = ~~tmpWorked.split(":")[1];
-workedHours = tmpWorkedHours + tmpWorkedMinutes / 60;
+const workedHours = (() => {
+  let tmpWorked = $(".infotpl td")[10].innerText;
+  let tmpWorkedHours = ~~tmpWorked.split(":")[0];
+  let tmpWorkedMinutes = ~~tmpWorked.split(":")[1];
+  return tmpWorkedHours + tmpWorkedMinutes / 60;
+})();
 
 // 有給休暇消化日数
-var tmpSalariedDays = 0,
-  tmpSalariedHarfDaysAm = 0,
-  tmpSalariedHarfDaysPm = 0;
-var thText;
-// 後ろから３カラム分のデータ（があるかもしれない）
-for (let i = 1; i < 4; i++) {
-  // ht の無いカラムは無いはずだが念のため
-  if (infotplRow[rowLength - i].getElementsByTagName("th")) {
-    thText = infotplRow[rowLength - i].getElementsByTagName("th")[0].innerText;
-  } else {
-    thText = null;
+const salariedDays = (() => {
+  let tmpSalariedDays = 0;
+  let tmpSalariedHarfDaysAm = 0;
+  let tmpSalariedHarfDaysPm = 0;
+
+  // 後ろから３カラム分のデータ（があるかもしれない）
+  for (let i = 1; i < 4; i++) {
+    let thText = !!$(".infotpl th")[rowLength - i]
+      ? $(".infotpl th")[rowLength - i].innerText
+      : null;
+    switch (thText) {
+      case "有休(全休)":
+        tmpSalariedDays = ~~infotplRow[rowLength - i].innerText;
+        break;
+      case "有休(AM休)":
+        tmpSalariedHarfDaysAm = ~~infotplRow[rowLength - i].innerText;
+        break;
+      case "有休(PM休)":
+        tmpSalariedHarfDaysPm = ~~infotplRow[rowLength - i].innerText;
+        break;
+      default:
+        break;
+    }
   }
-  // th の値によって判断する
-  switch (thText) {
-    case "有休(全休)":
-      tmpSalariedDays = ~~infotplRow[rowLength - i].getElementsByTagName(
-        "td"
-      )[0].innerText;
-      break;
-    case "有休(AM休)":
-      tmpSalariedHarfDaysAm = ~~infotplRow[rowLength - i].getElementsByTagName(
-        "td"
-      )[0].innerText;
-      break;
-    case "有休(PM休)":
-      tmpSalariedHarfDaysPm = ~~infotplRow[rowLength - i].getElementsByTagName(
-        "td"
-      )[0].innerText;
-      break;
-    default:
-      break;
-  }
-}
-salariedDays = tmpSalariedDays + tmpSalariedHarfDaysAm + tmpSalariedHarfDaysPm;
+  return tmpSalariedDays + tmpSalariedHarfDaysAm + tmpSalariedHarfDaysPm;
+})();
 
 // 今月の残り出勤可能日数
 // 所定労働日数から、(実働日数+有給(全休))を引いたもの
-remainWorkDays = stdWorkDays - (workedDays + salariedDays);
+const remainWorkDays = stdWorkDays - (workedDays + salariedDays);
 
 // 給与労働時間
 // 実労働時間に有給休暇（1 day = 8 hours とする）分の労働時間を足したもの
-excessWorkTimes = workedHours + salariedDays * 8;
+const excessWorkTimes = workedHours + salariedDays * 8;
 
 // 何時間余裕があるか
 // 残りの出勤可能日全て 8 時間労働すると仮定したときの猶予時間数
-workTimeMargin = excessWorkTimes - (stdWorkHours - remainWorkDays * 8);
+const workTimeMargin = excessWorkTimes - (stdWorkHours - remainWorkDays * 9);
 
 // 1日あたり何時間働けばいいか
 // 所定労働時間から実質労働時間を引いた値を残りの出勤可能日数で割ったもの
-requiredWorkTimes = (function() {
-  var shortTime = stdWorkHours - excessWorkTimes;
+const requiredWorkTimes = (() => {
+  let shortTime = stdWorkHours - excessWorkTimes;
   if (remainWorkDays == 0) {
     return shortTime;
   }
   return (stdWorkHours - excessWorkTimes) / remainWorkDays;
 })();
 
-lastUpdatedAt = Date.now();
+const lastUpdatedAt = Date.now();
 
-// DEBUG
-console.log(
-  "worktime.js",
-  stdWorkDays,
-  stdWorkHours,
-  workedDays,
-  workedHours,
-  salariedDays,
-  remainWorkDays,
-  excessWorkTimes,
-  workTimeMargin,
-  requiredWorkTimes,
-  lastUpdatedAt
-);
-
-var jobcanWorktimes = {
+const jobcanWorktimes = {
   state: {
     stdWorkDays: stdWorkDays,
     stdWorkHours: stdWorkHours,
@@ -125,4 +91,4 @@ var jobcanWorktimes = {
   }
 };
 
-chrome.storage.sync.set(jobcanWorktimes, function() {});
+chrome.storage.sync.set(jobcanWorktimes, () => {});
